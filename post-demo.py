1829824dab289d80fd2fc9bb42b0d467a6974586
yangmiao <yangmiao@mail.e-u.cn>
2018-01-12 13:26:02 +0800

    update ErrorHandle

--- post-demo.py
+++ post-demo.py
@@ -3,8 +3,6 @@ import requests
 import json
 
 s = requests
-data={"timestamp":5288970101,"longitude":104.3,"latitude":39.6}
-#data = json.dumps(data)
-#print("",type(data))
-r = s.post('http://192.168.200.136:5002/dataserver', data)
+data={"timestamp":1598965852,"longitude":108.6,"latitude":34.2}
+r = s.post('http://192.168.200.136:5005/dataserver', data)
 print(r.text)

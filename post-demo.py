24fd8f4f4bd06d448943c9aa1a12911ec5e9a724
maple <676715563@qq.com>
2018-01-15 13:38:12 +0800

    Update post-demo.py

--- post-demo.py
+++ post-demo.py
@@ -2,7 +2,6 @@
 import requests
 import json
 
-s = requests
 data={"timestamp":1598965852,"longitude":108.6,"latitude":34.2}
-r = s.post('http://192.168.200.136:5005/dataserver', data)
+r = requests.post('http://192.168.200.136:5005/dataserver', data)
 print(r.text)

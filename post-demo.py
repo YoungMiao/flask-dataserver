448d50b46512edbd6b0eed1351c65193acca5282
yangmiao <yangmiao@mail.e-u.cn>
2018-04-02 11:39:46 +0800

    delete old code

--- post-demo.py
+++
@@ -1,7 +0,0 @@
-#coding=utf-8
-import requests
-import json
-
-data={"timestamp":1598965852,"longitude":108.6,"latitude":34.2}
-r = requests.post('http://192.168.200.136:5005/dataserver', data)
-print(r.text)
